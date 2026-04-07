95e5eeba49316b4bee73c588acacec627ab2d91e
Sathira Sugeesvara <sathirasugeesvara@gmail.com>
2026-04-07 22:28:27 +0530

    Add project modal, certificates & skill icons

--- script.js
+++ script.js
@@ -26,3 +26,31 @@ document.addEventListener("click", (e) => {
         menuIcon.classList.replace("bx-x", "bx-menu");
     }
 });
+
+
+//for project section
+const modal = document.getElementById("projectModal");
+const modalImg = document.getElementById("modal-img");
+const modalTitle = document.getElementById("modal-title");
+const modalDesc = document.getElementById("modal-desc");
+const modalLink = document.getElementById("modal-link");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".modal-btn").forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const card = btn.closest(".project-card");
+        modalTitle.innerText = card.dataset.title;
+        modalDesc.innerText = card.dataset.desc;
+        modalImg.src = card.dataset.img;
+        modalLink.href = card.dataset.link;
+
+        modal.style.display = "block";
+    });
+});
+
+closeBtn.onclick = () => modal.style.display = "none";
+
+window.onclick = (e) => {
+    if (e.target == modal) modal.style.display = "none";
+};
